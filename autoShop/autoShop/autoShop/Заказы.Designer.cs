0c28e60861d39dd0cbe143c11e54073f54c4b884
0Sam120 <114755608+0Sam120@users.noreply.github.com>
2023-12-02 13:45:23 +0800

    Add files via upload

---
+++ autoShop/autoShop/autoShop/Заказы.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace autoShop
+{
+    partial class Заказы
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ordersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.autoShopDataSet = new autoShop.autoShopDataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ordersTableAdapter = new autoShop.autoShopDataSetTableAdapters.OrdersTableAdapter();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Марка = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ordersBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.ценаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clientIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.маркаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.датаЗаказаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autoShopDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ценаDataGridViewTextBoxColumn,
+            this.clientIDDataGridViewTextBoxColumn,
+            this.маркаDataGridViewTextBoxColumn,
+            this.датаЗаказаDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ordersBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(544, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ordersBindingSource
+            // 
+            this.ordersBindingSource.DataMember = "Orders";
+            this.ordersBindingSource.DataSource = this.autoShopDataSet;
+            // 
+            // autoShopDataSet
+            // 
+            this.autoShopDataSet.DataSetName = "autoShopDataSet";
+            this.autoShopDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<- Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ordersTableAdapter
+            // 
+            this.ordersTableAdapter.ClearBeforeFill = true;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(563, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(23, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Марка
+            // 
+            this.Марка.DataPropertyName = "Марка";
+            this.Марка.HeaderText = "Марка";
+            this.Марка.Name = "Марка";
+            this.Марка.ReadOnly = true;
+            // 
+            // ordersBindingSource1
+            // 
+            this.ordersBindingSource1.DataMember = "Orders";
+            this.ordersBindingSource1.DataSource = this.autoShopDataSet;
+            // 
+            // ценаDataGridViewTextBoxColumn
+            // 
+            this.ценаDataGridViewTextBoxColumn.DataPropertyName = "Цена";
+            this.ценаDataGridViewTextBoxColumn.HeaderText = "Цена";
+            this.ценаDataGridViewTextBoxColumn.Name = "ценаDataGridViewTextBoxColumn";
+            this.ценаDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // clientIDDataGridViewTextBoxColumn
+            // 
+            this.clientIDDataGridViewTextBoxColumn.DataPropertyName = "ClientID";
+            this.clientIDDataGridViewTextBoxColumn.HeaderText = "ClientID";
+            this.clientIDDataGridViewTextBoxColumn.Name = "clientIDDataGridViewTextBoxColumn";
+            this.clientIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.clientIDDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // маркаDataGridViewTextBoxColumn
+            // 
+            this.маркаDataGridViewTextBoxColumn.DataPropertyName = "Марка";
+            this.маркаDataGridViewTextBoxColumn.HeaderText = "Марка";
+            this.маркаDataGridViewTextBoxColumn.Name = "маркаDataGridViewTextBoxColumn";
+            this.маркаDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // датаЗаказаDataGridViewTextBoxColumn
+            // 
+            this.датаЗаказаDataGridViewTextBoxColumn.DataPropertyName = "Дата заказа";
+            this.датаЗаказаDataGridViewTextBoxColumn.HeaderText = "Дата заказа";
+            this.датаЗаказаDataGridViewTextBoxColumn.Name = "датаЗаказаDataGridViewTextBoxColumn";
+            this.датаЗаказаDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // Заказы
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(596, 215);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Заказы";
+            this.Text = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autoShopDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private autoShopDataSet autoShopDataSet;
+        private System.Windows.Forms.BindingSource ordersBindingSource;
+        private autoShopDataSetTableAdapters.OrdersTableAdapter ordersTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn клиентDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn модельDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn филиалDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn датаСделкиDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ценаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clientIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn маркаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn датаЗаказаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource ordersBindingSource1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Марка;
+    }
+}
---
+++ autoShop/autoShop/autoShop/ЛичныйКабинет.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace autoShop
+{
+    public partial class ЛичныйКабинет : Form
+    {
+        RegisteredUser CurrentUser;
+        public ЛичныйКабинет(RegisteredUser currentUser)
+        {
+            InitializeComponent();
+            CurrentUser = currentUser;
+
+            SqlConnection sqlConnect = new SqlConnection("Data Source=localhost;Initial Catalog = autoShop; Integrated Security = True");
+
+            try
+            {
+                sqlConnect.Open();
+                SqlCommand da = new SqlCommand("select * from Клиент where UserID = @login", sqlConnect);
+                da.Parameters.AddWithValue("@login", CurrentUser.UserID);
+                try
+                {
+                    using (SqlDataReader reader = da.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string lastName = reader["LastName"].ToString();
+                            string name = reader["Name"].ToString();
+                            string patronimic = reader["Patronimic"].ToString();
+                            string phone = reader["Phone"].ToString();
+                            string sign = reader["Sign"].ToString();
+                            string bank = reader["Bank"].ToString();
+                            string account = reader["Account"].ToString();
+
+                            // Отображение изображения на форме
+                            textBox1.Text = lastName;
+                            textBox2.Text = name;
+                            textBox3.Text = patronimic;
+                            textBox4.Text = phone;
+                            textBox5.Text = bank;
+                            textBox6.Text = account;
+                            if(sign == "1")
+                            {
+                                textBox7.Text = "Физическое Лицо";
+                            }
+                            else
+                            {
+                                textBox7.Text = "Юридическое Лицо";
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Изображение не найдено.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: " + ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Опибка при входе: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            МенюПокупателя brForm = new МенюПокупателя(CurrentUser);
+            brForm.Show();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Заказы orders = new Заказы(CurrentUser);
+            orders.Show();
+            this.Close();
+        }
+    }
+}
